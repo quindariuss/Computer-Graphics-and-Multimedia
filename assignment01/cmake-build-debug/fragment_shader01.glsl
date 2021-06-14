6a600ccc8403c6a6432e989c871dd84d84659b0b
Quin Lyles-Woods <qlyleswo@students.kennesaw.edu>
2021-06-14 18:01:07 -0400

    finished and added solution

--- assignment01/cmake-build-debug/fragment_shader01.glsl
+++ assignment01/cmake-build-debug/fragment_shader01.glsl
@@ -5,6 +5,6 @@ out vec4 fColor;
 void main()
 {
     // Dot Color
-    // Cant add
+    // Cant add the rgb values 105, green 89, blue 205, and alpha 255 directly because the shader program takes a floating point in the range of 0 - 1
     fColor = vec4(0.41, 0.35, 0.80, 1.0);
 }
