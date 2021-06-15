6930b850d2e9ce02fefe72c5278f231cccefa825
Quin Lyles-Woods <qlyleswo@students.kennesaw.edu>
2021-06-15 10:23:32 -0400

    changed the size to 200 in p3

--- assignment01/cmake-build-debug/fragment_shader02.glsl
+++ assignment01/cmake-build-debug/fragment_shader02.glsl
@@ -1,4 +1,5 @@
 #version 330 core
+// Changing the color to 105/255, 89/255, 205/255, and 255/255 made the color turn black so for some reason it will have turned all the values to zero
 
 out vec4 fColor;
 
