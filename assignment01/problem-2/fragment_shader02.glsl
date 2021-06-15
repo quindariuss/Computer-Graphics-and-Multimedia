2adf22b56de834f633eadf98aeccd2ba68bae02e
Quin Lyles-Woods <qlyleswo@students.kennesaw.edu>
2021-06-15 10:09:24 -0400

    changed the color to black as requested

--- assignment01/problem-2/fragment_shader02.glsl
+++ assignment01/problem-2/fragment_shader02.glsl
@@ -1,8 +1,9 @@
 #version 330 core
+// Changing the color to 105/255, 89/255, 205/255, and 255/255 made the color turn black so for some reason it will have turned all the values to zero
 
 out vec4 fColor;
 
 void main()
 {
-    fColor = vec4(1.0, 1.0, 0.0, 1.0);
+    fColor = vec4(105/255, 89/255, 205/255, 255/255);
 }
