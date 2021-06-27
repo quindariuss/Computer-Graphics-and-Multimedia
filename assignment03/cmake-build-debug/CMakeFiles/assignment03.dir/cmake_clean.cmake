af49775ab5ca856644c57a15e6613a2165ef25bb
Quindarius <quinwoodz@gmail.com>
2021-06-27 10:14:31 -0400

    assignment is running

---
+++ assignment03/cmake-build-debug/CMakeFiles/assignment03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment03.dir/assignment03/assignment03.cpp.obj"
+  "assignment03.exe"
+  "assignment03.exe.manifest"
+  "assignment03.pdb"
+  "libassignment03.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
